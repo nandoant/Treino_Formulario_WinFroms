fbf56c9c023b08334cfd1ddf9faffc417cffc7db
nandoant <fernandoantonio@protonmail.com>
2025-01-02 15:18:50 -0300

    Finalizado

--- Treino_Formulario_WinFroms/Calculadora de Aposentadoria.Designer.cs
+++ Treino_Formulario_WinFroms/Calculadora de Aposentadoria.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.form_Formulario = new System.Windows.Forms.GroupBox();
+            this.mTxtBox_AnosContrib = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton_Fem = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Masc = new System.Windows.Forms.RadioButton();
+            this.label_Sexo = new System.Windows.Forms.Label();
+            this.label_DataNascimento = new System.Windows.Forms.Label();
             this.textBox_Nome = new System.Windows.Forms.TextBox();
             this.btn_Confirmed = new System.Windows.Forms.Button();
             this.mTxtBox_CPF = new System.Windows.Forms.MaskedTextBox();
@@ -37,17 +43,17 @@
             this.label_nome = new System.Windows.Forms.Label();
             this.LinkLabel_aMinus = new System.Windows.Forms.LinkLabel();
             this.LinkLabel_aPlus = new System.Windows.Forms.LinkLabel();
-            this.label_DataNascimento = new System.Windows.Forms.Label();
             this.checkBox_Negrito = new System.Windows.Forms.CheckBox();
             this.checkBox_Italico = new System.Windows.Forms.CheckBox();
-            this.label_Sexo = new System.Windows.Forms.Label();
-            this.radioBtn_Masc = new System.Windows.Forms.RadioButton();
-            this.radioButton_Fem = new System.Windows.Forms.RadioButton();
+            this.label_TempContrib = new System.Windows.Forms.Label();
             this.form_Formulario.SuspendLayout();
             this.SuspendLayout();
             // 
             // form_Formulario
             // 
+            this.form_Formulario.Controls.Add(this.label_TempContrib);
+            this.form_Formulario.Controls.Add(this.mTxtBox_AnosContrib);
+            this.form_Formulario.Controls.Add(this.label1);
             this.form_Formulario.Controls.Add(this.radioButton_Fem);
             this.form_Formulario.Controls.Add(this.radioBtn_Masc);
             this.form_Formulario.Controls.Add(this.label_Sexo);
@@ -60,11 +66,74 @@
             this.form_Formulario.Controls.Add(this.label_nome);
             this.form_Formulario.Location = new System.Drawing.Point(12, 28);
             this.form_Formulario.Name = "form_Formulario";
-            this.form_Formulario.Size = new System.Drawing.Size(272, 286);
+            this.form_Formulario.Size = new System.Drawing.Size(272, 333);
             this.form_Formulario.TabIndex = 0;
             this.form_Formulario.TabStop = false;
             this.form_Formulario.Text = "Formulario";
             this.form_Formulario.Enter += new System.EventHandler(this.form_Formulario_Enter);
+            // 
+            // mTxtBox_AnosContrib
+            // 
+            this.mTxtBox_AnosContrib.Location = new System.Drawing.Point(10, 214);
+            this.mTxtBox_AnosContrib.Mask = "00";
+            this.mTxtBox_AnosContrib.Name = "mTxtBox_AnosContrib";
+            this.mTxtBox_AnosContrib.Size = new System.Drawing.Size(96, 22);
+            this.mTxtBox_AnosContrib.TabIndex = 20;
+            this.mTxtBox_AnosContrib.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mTxtBox_AnosContrib.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mTxtBox_AnosContrib_MaskInputRejected);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // radioButton_Fem
+            // 
+            this.radioButton_Fem.AutoSize = true;
+            this.radioButton_Fem.Location = new System.Drawing.Point(106, 267);
+            this.radioButton_Fem.Name = "radioButton_Fem";
+            this.radioButton_Fem.Size = new System.Drawing.Size(83, 20);
+            this.radioButton_Fem.TabIndex = 17;
+            this.radioButton_Fem.TabStop = true;
+            this.radioButton_Fem.Text = "Feminino";
+            this.radioButton_Fem.UseVisualStyleBackColor = true;
+            this.radioButton_Fem.CheckedChanged += new System.EventHandler(this.radioButton_Fem_CheckedChanged);
+            // 
+            // radioBtn_Masc
+            // 
+            this.radioBtn_Masc.AutoSize = true;
+            this.radioBtn_Masc.Location = new System.Drawing.Point(11, 267);
+            this.radioBtn_Masc.Name = "radioBtn_Masc";
+            this.radioBtn_Masc.Size = new System.Drawing.Size(89, 20);
+            this.radioBtn_Masc.TabIndex = 16;
+            this.radioBtn_Masc.TabStop = true;
+            this.radioBtn_Masc.Text = "Masculino";
+            this.radioBtn_Masc.UseVisualStyleBackColor = true;
+            this.radioBtn_Masc.CheckedChanged += new System.EventHandler(this.radioBtn_Masc_CheckedChanged);
+            // 
+            // label_Sexo
+            // 
+            this.label_Sexo.AutoSize = true;
+            this.label_Sexo.Location = new System.Drawing.Point(7, 248);
+            this.label_Sexo.Name = "label_Sexo";
+            this.label_Sexo.Size = new System.Drawing.Size(38, 16);
+            this.label_Sexo.TabIndex = 15;
+            this.label_Sexo.Text = "Sexo";
+            this.label_Sexo.Click += new System.EventHandler(this.label_Sexo_Click);
+            // 
+            // label_DataNascimento
+            // 
+            this.label_DataNascimento.AutoSize = true;
+            this.label_DataNascimento.Location = new System.Drawing.Point(7, 82);
+            this.label_DataNascimento.Name = "label_DataNascimento";
+            this.label_DataNascimento.Size = new System.Drawing.Size(130, 16);
+            this.label_DataNascimento.TabIndex = 14;
+            this.label_DataNascimento.Text = "Data de Nascimento";
+            this.label_DataNascimento.Click += new System.EventHandler(this.label_DataNascimento_Click);
             // 
             // textBox_Nome
             // 
@@ -76,7 +145,7 @@
             // 
             // btn_Confirmed
             // 
-            this.btn_Confirmed.Location = new System.Drawing.Point(187, 247);
+            this.btn_Confirmed.Location = new System.Drawing.Point(187, 295);
             this.btn_Confirmed.Name = "btn_Confirmed";
             this.btn_Confirmed.Size = new System.Drawing.Size(79, 29);
             this.btn_Confirmed.TabIndex = 12;
@@ -86,10 +155,10 @@
             // 
             // mTxtBox_CPF
             // 
-            this.mTxtBox_CPF.Location = new System.Drawing.Point(10, 162);
+            this.mTxtBox_CPF.Location = new System.Drawing.Point(10, 159);
             this.mTxtBox_CPF.Mask = "000.000.000-00";
             this.mTxtBox_CPF.Name = "mTxtBox_CPF";
-            this.mTxtBox_CPF.Size = new System.Drawing.Size(104, 22);
+            this.mTxtBox_CPF.Size = new System.Drawing.Size(96, 22);
             this.mTxtBox_CPF.TabIndex = 11;
             this.mTxtBox_CPF.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mTxtBox_CPF_MaskInputRejected);
             // 
@@ -98,7 +167,7 @@
             this.mTxtBox_DataNascimento.Location = new System.Drawing.Point(10, 101);
             this.mTxtBox_DataNascimento.Mask = "00/00/0000";
             this.mTxtBox_DataNascimento.Name = "mTxtBox_DataNascimento";
-            this.mTxtBox_DataNascimento.Size = new System.Drawing.Size(104, 22);
+            this.mTxtBox_DataNascimento.Size = new System.Drawing.Size(96, 22);
             this.mTxtBox_DataNascimento.TabIndex = 10;
             this.mTxtBox_DataNascimento.ValidatingType = typeof(System.DateTime);
             this.mTxtBox_DataNascimento.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mTxtBox_DataNascimento_MaskInputRejected);
@@ -106,7 +175,7 @@
             // label_CPF
             // 
             this.label_CPF.AutoSize = true;
-            this.label_CPF.Location = new System.Drawing.Point(7, 143);
+            this.label_CPF.Location = new System.Drawing.Point(7, 140);
             this.label_CPF.Name = "label_CPF";
             this.label_CPF.Size = new System.Drawing.Size(33, 16);
             this.label_CPF.TabIndex = 8;
@@ -145,16 +214,6 @@
             this.LinkLabel_aPlus.Text = "A+";
             this.LinkLabel_aPlus.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel_aPlus_LinkClicked);
             // 
-            // label_DataNascimento
-            // 
-            this.label_DataNascimento.AutoSize = true;
-            this.label_DataNascimento.Location = new System.Drawing.Point(7, 82);
-            this.label_DataNascimento.Name = "label_DataNascimento";
-            this.label_DataNascimento.Size = new System.Drawing.Size(130, 16);
-            this.label_DataNascimento.TabIndex = 14;
-            this.label_DataNascimento.Text = "Data de Nascimento";
-            this.label_DataNascimento.Click += new System.EventHandler(this.label_DataNascimento_Click);
-            // 
             // checkBox_Negrito
             // 
             this.checkBox_Negrito.AutoSize = true;
@@ -179,45 +238,21 @@
             this.checkBox_Italico.UseVisualStyleBackColor = true;
             this.checkBox_Italico.CheckedChanged += new System.EventHandler(this.checkBox_Italico_CheckedChanged);
             // 
-            // label_Sexo
+            // label_TempContrib
             // 
-            this.label_Sexo.AutoSize = true;
-            this.label_Sexo.Location = new System.Drawing.Point(7, 200);
-            this.label_Sexo.Name = "label_Sexo";
-            this.label_Sexo.Size = new System.Drawing.Size(38, 16);
-            this.label_Sexo.TabIndex = 15;
-            this.label_Sexo.Text = "Sexo";
-            this.label_Sexo.Click += new System.EventHandler(this.label_Sexo_Click);
-            // 
-            // radioBtn_Masc
-            // 
-            this.radioBtn_Masc.AutoSize = true;
-            this.radioBtn_Masc.Location = new System.Drawing.Point(11, 219);
-            this.radioBtn_Masc.Name = "radioBtn_Masc";
-            this.radioBtn_Masc.Size = new System.Drawing.Size(89, 20);
-            this.radioBtn_Masc.TabIndex = 16;
-            this.radioBtn_Masc.TabStop = true;
-            this.radioBtn_Masc.Text = "Masculino";
-            this.radioBtn_Masc.UseVisualStyleBackColor = true;
-            this.radioBtn_Masc.CheckedChanged += new System.EventHandler(this.radioBtn_Masc_CheckedChanged);
-            // 
-            // radioButton_Fem
-            // 
-            this.radioButton_Fem.AutoSize = true;
-            this.radioButton_Fem.Location = new System.Drawing.Point(106, 219);
-            this.radioButton_Fem.Name = "radioButton_Fem";
-            this.radioButton_Fem.Size = new System.Drawing.Size(83, 20);
-            this.radioButton_Fem.TabIndex = 17;
-            this.radioButton_Fem.TabStop = true;
-            this.radioButton_Fem.Text = "Feminino";
-            this.radioButton_Fem.UseVisualStyleBackColor = true;
-            this.radioButton_Fem.CheckedChanged += new System.EventHandler(this.radioButton_Fem_CheckedChanged);
+            this.label_TempContrib.AutoSize = true;
+            this.label_TempContrib.Location = new System.Drawing.Point(8, 195);
+            this.label_TempContrib.Name = "label_TempContrib";
+            this.label_TempContrib.Size = new System.Drawing.Size(135, 16);
+            this.label_TempContrib.TabIndex = 21;
+            this.label_TempContrib.Text = "Anos de Contribuição";
+            this.label_TempContrib.Click += new System.EventHandler(this.label_TempContrib_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 325);
+            this.ClientSize = new System.Drawing.Size(297, 371);
             this.Controls.Add(this.checkBox_Italico);
             this.Controls.Add(this.checkBox_Negrito);
             this.Controls.Add(this.LinkLabel_aPlus);
@@ -250,6 +285,9 @@
         private System.Windows.Forms.RadioButton radioButton_Fem;
         private System.Windows.Forms.RadioButton radioBtn_Masc;
         private System.Windows.Forms.Label label_Sexo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mTxtBox_AnosContrib;
+        private System.Windows.Forms.Label label_TempContrib;
     }
 }
 
--- Treino_Formulario_WinFroms/Calculadora de Aposentadoria.cs
+++ Treino_Formulario_WinFroms/Calculadora de Aposentadoria.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,10 @@ namespace Treino_Formulario_WinFroms
 {
     public partial class Form1 : Form
     {
+        String nome;
+        String dataNascimento;
+        String cpf;
+        String dataAtual = DateTime.Now.ToString().Split(' ')[0];
 
         public Form1()
         {
@@ -23,8 +28,8 @@ namespace Treino_Formulario_WinFroms
 
         }
 
-     //===== HEADER =====
-        //Tamanho texto
+//===== HEADER =====
+    //Tamanho texto
         private void LinkLabel_aPlus_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             foreach (Control crtl in form_Formulario.Controls)
@@ -36,7 +41,7 @@ namespace Treino_Formulario_WinFroms
                 crtl.Font = new Font(crtl.Font.FontFamily, crtl.Font.Size - 1, crtl.Font.Style);
         }
 
-        //Estilo da Fonte
+    //Estilo da Fonte
         private void checkBox_Italico_CheckedChanged(object sender, EventArgs e)
         {
             foreach (Control crtl in form_Formulario.Controls)
@@ -60,9 +65,9 @@ namespace Treino_Formulario_WinFroms
             }
         }
 
-        //===== FORM =====
+//===== FORM =====
 
-        //Nome
+    //Nome
         private void label_nome_Click(object sender, EventArgs e)
         {
 
@@ -73,7 +78,7 @@ namespace Treino_Formulario_WinFroms
 
         }
 
-        //Data de nascimento
+    //Data de nascimento
         private void label_DataNascimento_Click(object sender, EventArgs e)
         {
 
@@ -85,7 +90,7 @@ namespace Treino_Formulario_WinFroms
         }
 
 
-        //CPF
+    //CPF
 
         private void label_CPF_Click(object sender, EventArgs e)
         {
@@ -97,7 +102,7 @@ namespace Treino_Formulario_WinFroms
 
         }
 
-        //Sexo
+    //Sexo
         private void label_Sexo_Click(object sender, EventArgs e)
         {
 
@@ -113,20 +118,183 @@ namespace Treino_Formulario_WinFroms
 
         }
 
+    //Anos de Contribuição
+        private void label_TempContrib_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mTxtBox_AnosContrib_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
 
         //Confirmação
         private void btn_Confirmed_Click_1(object sender, EventArgs e)
         {
 
+            if (validateInputNome() && validateInputData() && validateInputTempContrib() && validadeInputSex())
+            {
+                int idadeRestante;
+                int contribRestante = (35 - int.Parse(mTxtBox_AnosContrib.Text)) < 0 ? 0 : (35 - int.Parse(mTxtBox_AnosContrib.Text));
+                int dif = 0;
+
+                if (radioBtn_Masc.Checked)
+                {
+                    dif = 65 - getInputIdade();
+                }
+
+                if (radioButton_Fem.Checked)
+                {
+                    dif = 60 - getInputIdade();
+                    
+                }
+                idadeRestante = dif < 0 ? 0 : dif;
+
+                MessageBox.Show($"Caro(a) {textBox_Nome.Text}, faltam {idadeRestante} anos para sua aposentadoria por idade e {contribRestante} anos de contribuição");
+
+            }
         }
 
+        private bool validadeInputSex()
+        {
+            label_Sexo.Text = "Sexo";
+            if(!radioBtn_Masc.Checked && !radioButton_Fem.Checked)
+            {
+                label_Sexo.Text += " (Selecione uma opção)";
+                return false;
+            }
+            return true;
+        }
+
+        private bool validateInputTempContrib()
+        {
+            try
+            {
+                label_TempContrib.Text = "Anos de Contribuição";
+                if (int.TryParse(mTxtBox_AnosContrib.Text, out int anosContrib))
+                {
+                    if (anosContrib > 0 && anosContrib < getInputIdade() - 16)
+                    {
+                        return true;
+                    }
+                }
+                label_TempContrib.Text += " (Invalido)";
+                return false;
+            } catch
+            {
+                return false;
+            }
+        }
+
+        private int getInputIdade()
+        {
+            try
+            {
+                int anoAtual = getAnoAtual();
+                int anoInput = int.Parse(getInputData().Split('/')[2]);
+                return anoAtual - anoInput;
+            } catch
+            {
+                throw new Exception("");
+            }
+        }
+
+        private bool validateInputNome()
+        {
+            label_nome.Text = "Nome";
+
+            String nome = textBox_Nome.Text.Trim();
+            nome = Regex.Replace(nome, "[^A-Za-zÀ-ÖØ-öø-ÿ]+", "");
+
+            if (nome.Length <= 0)
+            {
+                label_nome.Text += " (Invalido)";
+                return false;
+            }
+            return true;
+        }
+
+        //aux
+        private int getAnoAtual()
+        {
+            int ano = int.Parse(dataAtual.Split('/')[2]);
+            return ano;
+        }
+
+        private String getInputData()
+        {
+            return mTxtBox_DataNascimento.Text;
+        }
+
+        private Boolean validateInputData()
+        {
+            label_DataNascimento.Text = "Data de Nascimento";
+            if(validateInputDia() && validateInputMes() && validateInputAno())
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        private Boolean validateInputDia()
+        {
+            if (int.TryParse(mTxtBox_DataNascimento.Text.Split('/')[0], out int dia))
+            {
+                if (dia > 0 && dia < 31)
+                {
+                    return true;
+                }
+            }
+            label_DataNascimento.Text += " (Dia Invalido)";
+            return false;
+        }
+
+        private Boolean validateInputMes()
+        {
+            if (int.TryParse(mTxtBox_DataNascimento.Text.Split('/')[1], out int mes))
+            {
+                if (mes > 0 && mes < 13)
+                {
+                    return true;
+                }
+            }
+            label_DataNascimento.Text += " (Mes Invalido)";
+            return false;
+        }
+
+        private Boolean validateInputAno()
+        {
+            if (int.TryParse(mTxtBox_DataNascimento.Text.Split('/')[2], out int ano))
+            {
+                if (ano > 0 && ano < getAnoAtual())
+                {
+                    return true;
+                }
+            }
+            label_DataNascimento.Text += " (Ano Invalido)";
+            return false;
+        }
+
+        /*private int difInputDataEAnoAtual()
+        {
+            if()
+
+            return 0;
+        }
+        */
         //Outros
 
         private void form_Formulario_Enter(object sender, EventArgs e)
         {
-
+            
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
 
+        }
     }
 }
